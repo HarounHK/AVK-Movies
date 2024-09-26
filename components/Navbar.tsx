d84318d75af8a5541c76d8e823d17db8eb391b34
Haroun Kassouri <harounkassouri368@gmail.com>
2024-09-26 23:57:32 +0100

    Login/signup buttons skeleton made

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -19,25 +19,36 @@ export const Navbar = () => {
           </Link>
         </div>
         
-        <div className="cursor-pointer hidden md:flex space-x-10 items-center text-slate-300 text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50to bg-white text-white bg-opacity-50">
+        {/* <div className="cursor-pointer hidden md:flex space-x-10 items-center text-slate-300 text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50to bg-white text-white bg-opacity-50">
           <Link href="/" className="hover:text-gray-400">temp</Link>
           <Link href="/" className="hover:text-gray-400">temp</Link>
           <Link href="/" className="hover:text-gray-400">temp</Link>
           <Link href="/contact" className="hover:text-gray-400">temp</Link>
-        </div>
+        </div> */}
 
-        <div className="hidden md:flex">
-          <Link
-            href="/contact"
-            className="inline-flex h-12 animate-shimmer items-center justify-center 
-            rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] 
-            bg-[length:200%_100%] px-6 font-medium text-slate-400 transition-colors
-             focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2
-              focus:ring-offset-slate-50"
-          >
-            Contact
-          </Link>
-        </div>
+      <div className="hidden md:flex">
+        <Link
+          href="/"
+          className="inline-flex h-12 animate-shimmer items-center justify-center 
+          rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] 
+          bg-[length:200%_100%] px-6 font-medium text-slate-400 transition-colors
+          focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2
+          focus:ring-offset-slate-50 mr-4" // Add margin-right here
+        >
+          Login
+        </Link>
+        <Link
+          href="/"
+          className="inline-flex h-12 animate-shimmer items-center justify-center 
+          rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] 
+          bg-[length:200%_100%] px-6 font-medium text-slate-400 transition-colors
+          focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2
+          focus:ring-offset-slate-50"
+        >
+          Signup
+        </Link>
+      </div>
+
       </div>
     </div>
   );
