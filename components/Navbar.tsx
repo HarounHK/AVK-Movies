c5e2bcd5383c3b1443a7de49200389dc57b61fc5
Haroun Kassouri <harounkassouri368@gmail.com>
2024-09-26 03:31:24 +0100

    Added temp navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -18,8 +18,25 @@ export const Navbar = () => {
             />
           </Link>
         </div>
-        <div>
+        
+        <div className="cursor-pointer hidden md:flex space-x-10 items-center text-slate-300 text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50to bg-neutral-400 bg-opacity-50">
+          <Link href="/" className="hover:text-gray-50">temp</Link>
+          <Link href="/" className="hover:text-gray-50">temp</Link>
+          <Link href="/" className="hover:text-gray-50">temp</Link>
+          <Link href="/" className="hover:text-gray-50">temp</Link>
+        </div>
 
+        <div className="hidden md:flex">
+          <Link
+            href="/contact"
+            className="inline-flex h-12 animate-shimmer items-center justify-center 
+            rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] 
+            bg-[length:200%_100%] px-6 font-medium text-slate-400 transition-colors
+             focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2
+              focus:ring-offset-slate-50"
+          >
+            Contact
+          </Link>
         </div>
       </div>
     </div>
